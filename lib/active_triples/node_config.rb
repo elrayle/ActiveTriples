0ed9fb1e072d8fa7322ffc7cf97c4ff59918cb3f
E. Lynette Rayle <elr37@cornell.edu>
2015-06-26 15:35:16 -0400

    add property configurations for solr

--- lib/active_triples/node_config.rb
+++ lib/active_triples/node_config.rb
@@ -1,12 +1,17 @@
 module ActiveTriples
   class NodeConfig
-    attr_accessor :predicate, :term, :class_name, :type, :behaviors, :cast
+    attr_accessor :predicate, :term, :class_name, :type, :behaviors, :cast, :indexed, :sortable, :tokenize, :range, :multiple
 
     def initialize(term, predicate, args={})
       self.term = term
       self.predicate = predicate
       self.class_name = args.fetch(:class_name) { default_class_name }
       self.cast = args.fetch(:cast) { true }
+      self.indexed = args.fetch(:indexed) { false }
+      self.sortable = args.fetch(:sortable) { false }
+      self.tokenize = args.fetch(:tokenize) { false }
+      self.range = args.fetch(:range) { false }
+      self.multiple = args.fetch(:multiple) { false }
       yield(self) if block_given?
     end
 
